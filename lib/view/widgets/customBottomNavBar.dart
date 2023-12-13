7b29280c5c530a35ba580776f97d1348c60bff20
Asif Afroj Shayon <asifa@excelitai.com>
2023-12-13 23:53:12 +0600

    trying to splash off

--- lib/view/widgets/customBottomNavBar.dart
+++ lib/view/widgets/customBottomNavBar.dart
@@ -1,9 +1,5 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:hisab/provider/auth_provider.dart';
-import 'package:hisab/provider/noteProvider.dart';
 import 'package:hisab/view/screen/chart.dart';
 import 'package:hisab/view/screen/homepage.dart';
 import 'package:hisab/view/screen/profile.dart';
@@ -125,7 +121,7 @@ import '../screen/note.dart';
 // }
 ///provider\\\
 
-
+///ok
 class CurvedNavigationBarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -144,7 +140,7 @@ class CurvedNavigationBarPage extends StatelessWidget {
           );
         },
       ),
-      bottomNavigationBar: 
+      bottomNavigationBar:
       Padding(
         padding: EdgeInsets.only(bottom: 10.h,left: 15.w,right: 15.w),
         child: Consumer<BottomNavigationBarProvider>(
@@ -198,3 +194,79 @@ class CurvedNavigationBarPage extends StatelessWidget {
     );
   }
 }
+///
+// class CurvedNavigationBarPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: AppConst.appBackgroundColor,
+//       body: Consumer<BottomNavigationBarProvider>(
+//         builder: (context, provider, child) {
+//           return IndexedStack(
+//             index: provider.currentIndex,
+//             children: const [
+//               Homepage(),
+//               Note(),
+//               Chart(),
+//               Profile(),
+//             ],
+//           );
+//         },
+//       ),
+//       bottomNavigationBar: Padding(
+//         padding: EdgeInsets.only(bottom: 10.h, left: 15.w, right: 15.w),
+//         child: Consumer<BottomNavigationBarProvider>(
+//           builder: (context, provider, child) {
+//             return ClipRRect(
+//               borderRadius: BorderRadius.circular(40),
+//               child: Material(
+//                 color: Colors.transparent,
+//                 child: Container(
+//                   height: 86.h,
+//                   decoration: BoxDecoration(
+//                     borderRadius: BorderRadius.circular(40),
+//                     color: AppConst.appBackgroundColor,
+//                   ),
+//                   child: Row(
+//                     mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                     children: [
+//                       _buildNavItem(Icons.home, 'Home', provider, 0),
+//                       _buildNavItem(Icons.note, 'Note', provider, 1),
+//                       _buildNavItem(Icons.show_chart, 'Chart', provider, 2),
+//                       _buildNavItem(Icons.person, 'Profile', provider, 3),
+//                     ],
+//                   ),
+//                 ),
+//               ),
+//             );
+//           },
+//         ),
+//       ),
+//     );
+//   }
+//
+//   Widget _buildNavItem(IconData icon, String label, BottomNavigationBarProvider provider, int index) {
+//     return InkWell(
+//       splashColor: Colors.transparent,
+//       highlightColor: Colors.transparent,
+//       onTap: () {
+//         provider.updateIndex(index);
+//       },
+//       child: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           Icon(
+//             icon,
+//             color: provider.currentIndex == index ? Colors.blue : Colors.grey,
+//           ),
+//           Text(
+//             label,
+//             style: TextStyle(
+//               color: provider.currentIndex == index ? Colors.blue : Colors.grey,
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
