1922290ce1ef749ab69c7470e91787d8a6876838
Asif Afroj Shayon <asifa@excelitai.com>
2023-12-13 23:33:39 +0600

    custom navbar Finished with provider

--- lib/view/screen/chart.dart
+++ lib/view/screen/chart.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hisab/utils/app_constant.dart';
 class Chart extends StatelessWidget {
   const Chart({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppConst.appBackgroundColor,
     );
   }
 }
--- lib/view/widgets/customBottomNavBar.dart
+++ lib/view/widgets/customBottomNavBar.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hisab/provider/auth_provider.dart';
@@ -6,114 +8,193 @@ import 'package:hisab/view/screen/chart.dart';
 import 'package:hisab/view/screen/homepage.dart';
 import 'package:hisab/view/screen/profile.dart';
 import 'package:provider/provider.dart';
+import '../../provider/bottomNavigationBarProvider.dart';
 import '../../utils/app_constant.dart';
 import '../screen/note.dart';
 
-class CurvedNavigationBarPage extends StatefulWidget {
-  const CurvedNavigationBarPage({super.key});
+// class CurvedNavigationBarPage extends StatefulWidget {
+//   const CurvedNavigationBarPage({super.key});
+//
+//   @override
+//   State<CurvedNavigationBarPage> createState() =>
+//       _CurvedNavigationBarPageState();
+// }
 
-  @override
-  State<CurvedNavigationBarPage> createState() =>
-      _CurvedNavigationBarPageState();
-}
+//
+// class _CurvedNavigationBarPageState extends State<CurvedNavigationBarPage> {
+//   int _index = 0;
+//
+//   final List<Widget> _pages = [
+//     const Homepage(),
+//     const Note(),
+//     const Chart(),
+//     const Profile(),
+//   ];
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     final authProvider = Provider.of<AuthProvider>(context);
+//     final notesProvider = Provider.of<NotesProvider>(context);
+//
+//     return Scaffold(
+//       backgroundColor: AppConst.appBackgroundColor,
+//       body: _pages[_index],
+//       bottomNavigationBar: Padding(
+//         padding: EdgeInsets.only(bottom: 10.h),
+//         child: Container(
+//           height: 86.h,
+//           child:BottomNavigationBar(
+//           backgroundColor: Colors.transparent,
+//           type: BottomNavigationBarType.fixed,
+//           items: [
+//             BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home', backgroundColor: AppConst.appBackgroundColor),
+//             BottomNavigationBarItem(icon: Icon(Icons.note), label: 'Note', backgroundColor:AppConst.appBackgroundColor),
+//             BottomNavigationBarItem(icon: Icon(Icons.pie_chart_outline), label: 'Chart', backgroundColor: AppConst.appBackgroundColor),
+//             BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile', backgroundColor: AppConst.appBackgroundColor),
+//           ],
+//           onTap: (index) {
+//             setState(() {
+//               _index = index;
+//             });
+//           },
+//         ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+///set state style ///
+// class _CurvedNavigationBarPageState extends State<CurvedNavigationBarPage> {
+//   int _index = 0;
+//
+//   final List<Widget> _pages = [
+//     const Homepage(),
+//     const Note(),
+//     const Chart(),
+//     const Profile(),
+//   ];
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     final authProvider = Provider.of<AuthProvider>(context);
+//     final notesProvider = Provider.of<NotesProvider>(context);
+//
+//     return Scaffold(
+//       backgroundColor: AppConst.appBackgroundColor,
+//       body: _pages[_index],
+//       bottomNavigationBar: Padding(
+//         padding: EdgeInsets.only(bottom: 10.h),
+//         child: Container(
+//           height: 86.h,
+//           child: BottomNavigationBar(
+//             backgroundColor: Colors.transparent,
+//             type: BottomNavigationBarType.fixed,
+//             items: [
+//               BottomNavigationBarItem(
+//                 icon: Icon(Icons.home),
+//                 label: 'Home',
+//                 backgroundColor: AppConst.appBackgroundColor,
+//               ),
+//               BottomNavigationBarItem(
+//                 icon: Icon(Icons.note),
+//                 label: 'Note',
+//                 backgroundColor: AppConst.appBackgroundColor,
+//               ),
+//               BottomNavigationBarItem(
+//                 icon: Icon(Icons.pie_chart_outline),
+//                 label: 'Chart',
+//                 backgroundColor: AppConst.appBackgroundColor,
+//               ),
+//               BottomNavigationBarItem(
+//                 icon: Icon(Icons.person),
+//                 label: 'Profile',
+//                 backgroundColor: AppConst.appBackgroundColor,
+//               ),
+//             ],
+//             currentIndex: _index, // Add this line to set the current index
+//             onTap: (index) {
+//               setState(() {
+//                 _index = index;
+//               });
+//             },
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+///provider\\\
 
-class _CurvedNavigationBarPageState extends State<CurvedNavigationBarPage> {
-  int _index = 0;
 
-  final List<Widget> _pages = [
-    const Homepage(),
-    const Note(),
-    const Chart(),
-    const Profile(),
-  ];
-
+class CurvedNavigationBarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final authProvider = Provider.of<AuthProvider>(context);
-    final notesProvider = Provider.of<NotesProvider>(context);
-
     return Scaffold(
       backgroundColor: AppConst.appBackgroundColor,
-      body: _pages[_index],
-      bottomNavigationBar: Padding(
-        padding: EdgeInsets.only(bottom: 10.h),
-        child: Container(
-          height: 86.h,
-          // child: BottomNavigationBar(
-          //   backgroundColor: Colors.transparent,
-          //   items: [
-          //     BottomNavigationBarItem(
-          //       icon: Icon(Icons.home),
-          //       label: 'Home',
-          //       backgroundColor: AppConst.appBackgroundColor,
-          //       type: BottomNavigationBarType.fixed,
-          //     ),
-          //     BottomNavigationBarItem(
-          //       icon: Icon(Icons.note),
-          //       label: 'Note',
-          //       backgroundColor: Colors.white,
-          //       type: BottomNavigationBarType.fixed,
-          //     ),
-          //     BottomNavigationBarItem(
-          //       icon: Icon(Icons.pie_chart_outline),
-          //       label: 'Chart',
-          //       backgroundColor: AppConst.appBackgroundColor,
-          //       type: BottomNavigationBarType.fixed,
-          //     ),
-          //     BottomNavigationBarItem(
-          //       icon: Icon(Icons.person),
-          //       label: 'Profile',
-          //       backgroundColor: AppConst.appBackgroundColor,
-          //       type: BottomNavigationBarType.fixed,
-          //     ),
-          //     // BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home', backgroundColor: AppConst.appBackgroundColor),
-          //     // BottomNavigationBarItem(icon: Icon(Icons.note), label: 'Note', backgroundColor: Colors.white, textOverflow: TextOverflow.visible),
-          //     // BottomNavigationBarItem(icon: Icon(Icons.pie_chart_outline), label: 'Chart', backgroundColor: AppConst.appBackgroundColor, textOverflow: TextOverflow.visible),
-          //     // BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile', backgroundColor: AppConst.appBackgroundColor, textOverflow: TextOverflow.visible),
-          //         // Icon(Icons.ice_skating, size: 24.sp, color: Colors.green),
-          //         // Icon(Icons.note, size: 24.sp, color: Colors.green),
-          //         // Icon(Icons.pie_chart_outline, size: 24.sp, color: Colors.green),
-          //         // Icon(Icons.person, size: 24.sp, color: Colors.green),
-          //   ],
-          //     onTap: (index) {
-          //   setState(() {
-          //     _index = index;
-          //   });
-          // },
-          //     ),
-          child:BottomNavigationBar(
-          backgroundColor: Colors.transparent,
-          type: BottomNavigationBarType.fixed,
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home', backgroundColor: AppConst.appBackgroundColor),
-            BottomNavigationBarItem(icon: Icon(Icons.note), label: 'Note', backgroundColor:AppConst.appBackgroundColor),
-            BottomNavigationBarItem(icon: Icon(Icons.pie_chart_outline), label: 'Chart', backgroundColor: AppConst.appBackgroundColor),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile', backgroundColor: AppConst.appBackgroundColor),
-          ],
-          onTap: (index) {
-            setState(() {
-              _index = index;
-            });
+      body: Consumer<BottomNavigationBarProvider>(
+        builder: (context, provider, child) {
+          return IndexedStack(
+            index: provider.currentIndex,
+            children: const [
+              Homepage(),
+              Note(),
+              Chart(),
+              Profile(),
+            ],
+          );
+        },
+      ),
+      bottomNavigationBar: 
+      Padding(
+        padding: EdgeInsets.only(bottom: 10.h,left: 15.w,right: 15.w),
+        child: Consumer<BottomNavigationBarProvider>(
+          builder: (context, provider, child) {
+            return ClipRRect(
+              borderRadius: BorderRadius.circular(40),
+              child: Container(
+                height: 86.h,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(40),
+                  color: AppConst.appBackgroundColor
+                ),
+                child: BottomNavigationBar(
+                  backgroundColor: Colors.transparent,
+                  type: BottomNavigationBarType.fixed,
+                  currentIndex: provider.currentIndex,
+                  // selectedItemColor: Colors.blue, // Set the color for the selected item
+                  unselectedItemColor: Colors.grey, // Set
+                  selectedLabelStyle: AppConst.hisabTextStyle,
+                  items: [
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.home),
+                      label: 'Home',
+                      backgroundColor: AppConst.appBackgroundColor,
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.note),
+                      label: 'Note',
+                      backgroundColor: AppConst.appBackgroundColor,
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.show_chart),
+                      label: 'Chart',
+                      backgroundColor: AppConst.appBackgroundColor,
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.person),
+                      label: 'Profile',
+                      backgroundColor: AppConst.appBackgroundColor,
+                    ),
+                  ],
+                  onTap: (index) {
+                    Provider.of<BottomNavigationBarProvider>(context, listen: false).updateIndex(index);
+                  },
+                ),
+              ),
+            );
           },
         ),
-        ),
       ),
-      
-      // CurvedNavigationBar(
-      //   index: _index,
-      //   backgroundColor: Colors.transparent,
-      //   items: <Widget>[
-      //     Icon(Icons.home, size: 24.sp, color: Colors.green),
-      //     Icon(Icons.note, size: 24.sp, color: Colors.green),
-      //     Icon(Icons.pie_chart_outline, size: 24.sp, color: Colors.green),
-      //     Icon(Icons.person, size: 24.sp, color: Colors.green),
-      //   ],
-      //   onTap: (index) {
-      //     setState(() {
-      //       _index = index;
-      //     });
-      //   },
-      // ),
     );
   }
 }
