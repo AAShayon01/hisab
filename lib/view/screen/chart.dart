2f3ebff2d6cfab050c7f7409d6e27efb6dbea9d9
asif-excelitai <asifa@excelitai.com>
2023-12-14 13:15:55 +0600

    homepage.dart ,ring fixes

--- lib/view/screen/chart.dart
+++ lib/view/screen/chart.dart
@@ -124,119 +124,139 @@
 //   }
 // }
 
+// import 'package:flutter/material.dart';
+// import 'package:fl_chart/fl_chart.dart';
+// import 'package:provider/provider.dart';
+//
+// import '../../data/model/chartEntry.dart';
+// import '../../provider/chartProvider.dart';// Import your actual path to the provider file
+//
+// class Chart extends StatelessWidget {
+//   final TextEditingController _monthController = TextEditingController();
+//   final TextEditingController _incomeController = TextEditingController();
+//   final TextEditingController _expenseController = TextEditingController();
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Monthly Chart'),
+//       ),
+//       body: Consumer<ChartDataProvider>(
+//         builder: (context, chartProvider, child) {
+//           final chartData = chartProvider.chartData;
+//
+//           return Padding(
+//             padding: const EdgeInsets.all(16.0),
+//             child: LineChart(
+//               LineChartData(
+//                 lineBarsData: [
+//                   LineChartBarData(
+//                     spots: chartData
+//                         .asMap()
+//                         .entries
+//                         .map((entry) => FlSpot(entry.key.toDouble(), entry.value.income))
+//                         .toList(),
+//                     isCurved: true,
+//                     colors: [Colors.blue],
+//                     dotData: FlDotData(show: false),
+//                     belowBarData: BarAreaData(show: false),
+//                     isStrokeCapRound: true, // Add this line
+//                   ),
+//                   LineChartBarData(
+//                     spots: chartData
+//                         .asMap()
+//                         .entries
+//                         .map((entry) => FlSpot(entry.key.toDouble(), entry.value.expense))
+//                         .toList(),
+//                     isCurved: true,
+//                     colors: [Colors.red],
+//                     dotData: FlDotData(show: false),
+//                     belowBarData: BarAreaData(show: false),
+//                     isStrokeCapRound: true, // Add this line
+//                   ),
+//                 ],
+//                 titlesData: FlTitlesData(
+//                   leftTitles: SideTitles(showTitles: true),
+//                   bottomTitles: SideTitles(
+//                     showTitles: true,
+//                     getTitles: (value) {
+//                       final index = value.toInt();
+//                       if (index >= 0 && index < chartData.length) {
+//                         return chartData[index].month;
+//                       }
+//                       return '';
+//                     },
+//                   ),
+//                 ),
+//                 gridData: FlGridData(show: true),
+//               ),
+//             ),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+//
+//   Widget _buildForm(BuildContext context) {
+//     final chartProvider = Provider.of<ChartDataProvider>(context, listen: false);
+//
+//     return Form(
+//       child: Column(
+//         children: [
+//           TextFormField(
+//             controller: _monthController,
+//             decoration: InputDecoration(labelText: 'Month'),
+//           ),
+//           TextFormField(
+//             controller: _incomeController,
+//             keyboardType: TextInputType.number,
+//             decoration: InputDecoration(labelText: 'Income'),
+//           ),
+//           TextFormField(
+//             controller: _expenseController,
+//             keyboardType: TextInputType.number,
+//             decoration: InputDecoration(labelText: 'Expense'),
+//           ),
+//           SizedBox(height: 8),
+//           ElevatedButton(
+//             onPressed: () {
+//               final month = _monthController.text;
+//               final income = double.parse(_incomeController.text);
+//               final expense = double.parse(_expenseController.text);
+//
+//               final newEntry = ChartEntry(month: month, income: income, expense: expense);
+//               chartProvider.setChartData([...chartProvider.chartData, newEntry]);
+//
+//               _monthController.clear();
+//               _incomeController.clear();
+//               _expenseController.clear();
+//             },
+//             child: Text('Add Data'),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
-import 'package:fl_chart/fl_chart.dart';
-import 'package:provider/provider.dart';
-
-import '../../data/model/chartEntry.dart';
-import '../../provider/chartProvider.dart';// Import your actual path to the provider file
-
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hisab/utils/app_constant.dart';
 class Chart extends StatelessWidget {
-  final TextEditingController _monthController = TextEditingController();
-  final TextEditingController _incomeController = TextEditingController();
-  final TextEditingController _expenseController = TextEditingController();
+  const Chart({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Monthly Chart'),
-      ),
-      body: Consumer<ChartDataProvider>(
-        builder: (context, chartProvider, child) {
-          final chartData = chartProvider.chartData;
-
-          return Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: LineChart(
-              LineChartData(
-                lineBarsData: [
-                  LineChartBarData(
-                    spots: chartData
-                        .asMap()
-                        .entries
-                        .map((entry) => FlSpot(entry.key.toDouble(), entry.value.income))
-                        .toList(),
-                    isCurved: true,
-                    colors: [Colors.blue],
-                    dotData: FlDotData(show: false),
-                    belowBarData: BarAreaData(show: false),
-                    isStrokeCapRound: true, // Add this line
-                  ),
-                  LineChartBarData(
-                    spots: chartData
-                        .asMap()
-                        .entries
-                        .map((entry) => FlSpot(entry.key.toDouble(), entry.value.expense))
-                        .toList(),
-                    isCurved: true,
-                    colors: [Colors.red],
-                    dotData: FlDotData(show: false),
-                    belowBarData: BarAreaData(show: false),
-                    isStrokeCapRound: true, // Add this line
-                  ),
-                ],
-                titlesData: FlTitlesData(
-                  leftTitles: SideTitles(showTitles: true),
-                  bottomTitles: SideTitles(
-                    showTitles: true,
-                    getTitles: (value) {
-                      final index = value.toInt();
-                      if (index >= 0 && index < chartData.length) {
-                        return chartData[index].month;
-                      }
-                      return '';
-                    },
-                  ),
-                ),
-                gridData: FlGridData(show: true),
-              ),
-            ),
-          );
-        },
-      ),
-    );
-  }
-}
-
-  Widget _buildForm(BuildContext context) {
-    final chartProvider = Provider.of<ChartDataProvider>(context, listen: false);
-
-    return Form(
-      child: Column(
-        children: [
-          TextFormField(
-            controller: _monthController,
-            decoration: InputDecoration(labelText: 'Month'),
+      backgroundColor: AppConst.appBackgroundColor,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: Padding(
+            padding: EdgeInsets.only(left: 10.w),
+            child:  Text('Chart',style: AppConst.appTextStyle,),
           ),
-          TextFormField(
-            controller: _incomeController,
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(labelText: 'Income'),
-          ),
-          TextFormField(
-            controller: _expenseController,
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(labelText: 'Expense'),
-          ),
-          SizedBox(height: 8),
-          ElevatedButton(
-            onPressed: () {
-              final month = _monthController.text;
-              final income = double.parse(_incomeController.text);
-              final expense = double.parse(_expenseController.text);
-
-              final newEntry = ChartEntry(month: month, income: income, expense: expense);
-              chartProvider.setChartData([...chartProvider.chartData, newEntry]);
-
-              _monthController.clear();
-              _incomeController.clear();
-              _expenseController.clear();
-            },
-            child: Text('Add Data'),
-          ),
-        ],
-      ),
+        )
     );
   }
 }
--- lib/view/widgets/customBottomNavBar.dart
+++ lib/view/widgets/customBottomNavBar.dart
@@ -122,79 +122,6 @@ import '../screen/note.dart';
 ///provider\\\
 
 ///ok
-class CurvedNavigationBarPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppConst.appBackgroundColor,
-      body: Consumer<BottomNavigationBarProvider>(
-        builder: (context, provider, child) {
-          return IndexedStack(
-            index: provider.currentIndex,
-            children:  [
-              Homepage(),
-              Note(),
-              Chart(),
-              Profile(),
-            ],
-          );
-        },
-      ),
-      bottomNavigationBar:
-      Padding(
-        padding: EdgeInsets.only(bottom: 10.h,left: 15.w,right: 15.w),
-        child: Consumer<BottomNavigationBarProvider>(
-          builder: (context, provider, child) {
-            return ClipRRect(
-              borderRadius: BorderRadius.circular(40),
-              child: Container(
-                height: 86.h,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(40),
-                  color: AppConst.appBackgroundColor
-                ),
-                child: BottomNavigationBar(
-                  backgroundColor: Colors.transparent,
-                  type: BottomNavigationBarType.fixed,
-                  currentIndex: provider.currentIndex,
-                  // selectedItemColor: Colors.blue, // Set the color for the selected item
-                  unselectedItemColor: Colors.grey, // Set
-                  selectedLabelStyle: AppConst.hisabTextStyle,
-                  items: [
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.home),
-                      label: 'Home',
-                      backgroundColor: AppConst.appBackgroundColor,
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.note),
-                      label: 'Note',
-                      backgroundColor: AppConst.appBackgroundColor,
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.show_chart),
-                      label: 'Chart',
-                      backgroundColor: AppConst.appBackgroundColor,
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.person),
-                      label: 'Profile',
-                      backgroundColor: AppConst.appBackgroundColor,
-                    ),
-                  ],
-                  onTap: (index) {
-                    Provider.of<BottomNavigationBarProvider>(context, listen: false).updateIndex(index);
-                  },
-                ),
-              ),
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
-///
 // class CurvedNavigationBarPage extends StatelessWidget {
 //   @override
 //   Widget build(BuildContext context) {
@@ -204,7 +131,7 @@ class CurvedNavigationBarPage extends StatelessWidget {
 //         builder: (context, provider, child) {
 //           return IndexedStack(
 //             index: provider.currentIndex,
-//             children: const [
+//             children:  [
 //               Homepage(),
 //               Note(),
 //               Chart(),
@@ -213,29 +140,51 @@ class CurvedNavigationBarPage extends StatelessWidget {
 //           );
 //         },
 //       ),
-//       bottomNavigationBar: Padding(
-//         padding: EdgeInsets.only(bottom: 10.h, left: 15.w, right: 15.w),
+//       bottomNavigationBar:
+//       Padding(
+//         padding: EdgeInsets.only(bottom: 10.h,left: 15.w,right: 15.w),
 //         child: Consumer<BottomNavigationBarProvider>(
 //           builder: (context, provider, child) {
 //             return ClipRRect(
 //               borderRadius: BorderRadius.circular(40),
-//               child: Material(
-//                 color: Colors.transparent,
-//                 child: Container(
-//                   height: 86.h,
-//                   decoration: BoxDecoration(
-//                     borderRadius: BorderRadius.circular(40),
-//                     color: AppConst.appBackgroundColor,
-//                   ),
-//                   child: Row(
-//                     mainAxisAlignment: MainAxisAlignment.spaceAround,
-//                     children: [
-//                       _buildNavItem(Icons.home, 'Home', provider, 0),
-//                       _buildNavItem(Icons.note, 'Note', provider, 1),
-//                       _buildNavItem(Icons.show_chart, 'Chart', provider, 2),
-//                       _buildNavItem(Icons.person, 'Profile', provider, 3),
-//                     ],
-//                   ),
+//               child: Container(
+//                 height: 86.h,
+//                 decoration: BoxDecoration(
+//                   borderRadius: BorderRadius.circular(40),
+//                   color: AppConst.appBackgroundColor
+//                 ),
+//                 child: BottomNavigationBar(
+//                   backgroundColor: Colors.transparent,
+//                   type: BottomNavigationBarType.fixed,
+//                   currentIndex: provider.currentIndex,
+//                   // selectedItemColor: Colors.blue, // Set the color for the selected item
+//                   unselectedItemColor: Colors.grey, // Set
+//                   selectedLabelStyle: AppConst.hisabTextStyle,
+//                   items: [
+//                     BottomNavigationBarItem(
+//                       icon: Icon(Icons.home),
+//                       label: 'Home',
+//                       backgroundColor: AppConst.appBackgroundColor,
+//                     ),
+//                     BottomNavigationBarItem(
+//                       icon: Icon(Icons.note),
+//                       label: 'Note',
+//                       backgroundColor: AppConst.appBackgroundColor,
+//                     ),
+//                     BottomNavigationBarItem(
+//                       icon: Icon(Icons.show_chart),
+//                       label: 'Chart',
+//                       backgroundColor: AppConst.appBackgroundColor,
+//                     ),
+//                     BottomNavigationBarItem(
+//                       icon: Icon(Icons.person),
+//                       label: 'Profile',
+//                       backgroundColor: AppConst.appBackgroundColor,
+//                     ),
+//                   ],
+//                   onTap: (index) {
+//                     Provider.of<BottomNavigationBarProvider>(context, listen: false).updateIndex(index);
+//                   },
 //                 ),
 //               ),
 //             );
@@ -244,29 +193,80 @@ class CurvedNavigationBarPage extends StatelessWidget {
 //       ),
 //     );
 //   }
-//
-//   Widget _buildNavItem(IconData icon, String label, BottomNavigationBarProvider provider, int index) {
-//     return InkWell(
-//       splashColor: Colors.transparent,
-//       highlightColor: Colors.transparent,
-//       onTap: () {
-//         provider.updateIndex(index);
-//       },
-//       child: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           Icon(
-//             icon,
-//             color: provider.currentIndex == index ? Colors.blue : Colors.grey,
-//           ),
-//           Text(
-//             label,
-//             style: TextStyle(
-//               color: provider.currentIndex == index ? Colors.blue : Colors.grey,
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
 // }
+///
+class CurvedNavigationBarPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppConst.appBackgroundColor,
+      body: Consumer<BottomNavigationBarProvider>(
+        builder: (context, provider, child) {
+          return IndexedStack(
+            index: provider.currentIndex,
+            children: const [
+              Homepage(),
+              Note(),
+              Chart(),
+              Profile(),
+            ],
+          );
+        },
+      ),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.only(bottom: 10.h, left: 15.w, right: 15.w),
+        child: Consumer<BottomNavigationBarProvider>(
+          builder: (context, provider, child) {
+            return ClipRRect(
+              borderRadius: BorderRadius.circular(40),
+              child: Material(
+                color: Colors.transparent,
+                child: Container(
+                  height: 86.h,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    color: AppConst.appBackgroundColor,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      _buildNavItem(Icons.home, 'Home', provider, 0),
+                      _buildNavItem(Icons.note, 'Note', provider, 1),
+                      _buildNavItem(Icons.show_chart, 'Chart', provider, 2),
+                      _buildNavItem(Icons.person, 'Profile', provider, 3),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNavItem(IconData icon, String label, BottomNavigationBarProvider provider, int index) {
+    return InkWell(
+      splashColor: Colors.transparent,
+      highlightColor: Colors.transparent,
+      onTap: () {
+        provider.updateIndex(index);
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            color: provider.currentIndex == index ? Colors.blue : Colors.grey,
+          ),
+          Text(
+            label,
+            style: TextStyle(
+              color: provider.currentIndex == index ? Colors.blue : Colors.grey,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
